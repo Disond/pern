cd8be2847fd93e425974866409dc807d8e48e73e
Disond <milosevic.petar4@gmail.com>
2026-02-24 12:29:19 +0100

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { ENV } from "./config/env";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message:
+            "Welcome to PERN API  - Powered by PostgreSQL, Drizzle ORM & Better Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        },
+    });
+});
+
+app.listen(ENV.PORT, () =>
+    console.log("Server is up and running on PORT: ", ENV.PORT),
+);
